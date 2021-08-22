54e53fdf951959f5e6f691864410f090695d492c
pcsmomo <dongwoo_333@hanmail.net>
2021-08-22 19:57:13 +1000

    front/webpack: minor config structure change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,76 +4,78 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle[contenthash].js',
-    path: path.resolve(__dirname, './build')
-  },
-  mode: 'none',
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|jpeg)$/,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: {
-            maxSize: 3 * 1024 // 3 kilobytes, The default size is 8kb
+module.exports = (webpackEnv) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle[contenthash].js',
+      path: path.resolve(__dirname, './build')
+    },
+    mode: 'none',
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpg|jpeg)$/,
+          type: 'asset',
+          parser: {
+            dataUrlCondition: {
+              maxSize: 3 * 1024 // 3 kilobytes, The default size is 8kb
+            }
+          }
+        },
+        {
+          test: /\.txt/,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      },
-      {
-        test: /\.txt/,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'styles[contenthash].css'
-    }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        '**/*',
-        path.join(process.cwd(), 'build/**/*')
       ]
-    }),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true
-      }
-    })
-  ]
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles[contenthash].css'
+      }),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [
+          '**/*',
+          path.join(process.cwd(), 'build/**/*')
+        ]
+      }),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true
+        }
+      })
+    ]
+  };
 };
